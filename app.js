3564dda835f54d5f1448c2a215fc2c186439163a
Icko92 <denkovski.ivica.92@gmail.com>
2021-03-05 13:57:40 +0100

    rendered days elements

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("icko");
+import weatherData from "./weatherData.js";
+const days = weatherData.days;
+console.log(weatherData);
+
+// dom selectors
+const daysWrap = document.querySelector(".days-wrap");
+
+// create element
+const daysList = document.createElement("ul");
+
+//render days
+days.forEach((day, index) => {
+  // create elements
+  const dayCard = document.createElement("li");
+  const weatherIcon = document.createElement("i");
+  const dayWeatherInfo = document.createElement("div");
+  const dayName = document.createElement("p");
+  const dayTemperature = document.createElement("p");
+
+  //add attributes
+  weatherIcon.setAttribute("class", "fas fa-sun");
+  dayWeatherInfo.setAttribute("class", "day-weather-info");
+
+  //add content
+  dayName.textContent = day.day;
+  dayTemperature.innerHTML = day.temp + "&#176";
+
+  //append elements
+  daysWrap.appendChild(daysList);
+  daysList.appendChild(dayCard);
+  dayCard.appendChild(weatherIcon);
+  dayCard.appendChild(dayWeatherInfo);
+  dayWeatherInfo.appendChild(dayName);
+  dayWeatherInfo.appendChild(dayTemperature);
+});
---
+++ weatherData.js
@@ -0,0 +1,57 @@
+const weatherData = {
+  tempUnit: "C",
+  windSpeedUnit: "m/s",
+  days: [
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny",
+    },
+  ],
+};
+
+export default weatherData;
